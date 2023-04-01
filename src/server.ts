4f0be10b3ef73ae01bb39cca9dfacf60915c2009
erick2014 <erickarmandogarcia@hotmail.com>
2023-04-01 18:15:55 -0500

    improve type for schemas

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { getResolversAndSchemas } from "./utils.ts";
 import { mergeSchemas } from "@graphql-tools/schema";
+import { GraphQLSchema } from "graphql";
 
-const schemas: any = await getResolversAndSchemas();
+const schemas: GraphQLSchema[] = await getResolversAndSchemas();
 const server = new ApolloServer({
   schema: mergeSchemas({ schemas }),
 });
